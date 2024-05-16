76c43951b02831d1e120f7c02e3d8cbdb8bff76e
Glenn Chiang <glennchiang24@gmail.com>
2024-05-16 10:46:11 +0800

    Make safe zone expire after 5 seconds

--- AstroMania/Assets/Zones/SafeZone.cs
+++ AstroMania/Assets/Zones/SafeZone.cs
@@ -2,9 +2,21 @@ using UnityEngine;
 
 public class SafeZone : MonoBehaviour
 {
+    [SerializeField] private float activeDuration;
+
+    private void Awake()
+    {
+        Invoke(nameof(Expire), activeDuration);
+    }
+
+    private void Expire()
+    {
+        Destroy(gameObject);
+    }
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.CompareTag("Enemy"))
+        if (collider.CompareTag("Enemy") || collider.TryGetComponent<ProjectileController>(out var projectile))
         {
             Destroy(collider.gameObject);
         }
